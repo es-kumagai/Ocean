b0fbf4be40e87e8b7204372e088ac225c7508bc4
Tomohiro Kumagai <tomohiro@ez-style.jp>
2017-12-13 05:39:28 +0900

    Adding .

---
+++ Sources/ReferenceCounter.swift
@@ -0,0 +1,58 @@
+//
+//  ReferenceCounter.swift
+//  Swim
+//
+//  Created by Tomohiro Kumagai on 12/13/17.
+//
+
+import Dispatch
+
+public struct ReferenceCounter {
+    
+    private var _count: Int
+    
+    public init() {
+    
+        _count = 0
+    }
+
+    public var count: Int {
+    
+        return ReferenceCounter.queue.sync {
+            
+            return _count
+        }
+    }
+    
+    public var retained: Bool {
+        
+        return ReferenceCounter.queue.sync {
+
+            _count != 0
+        }
+    }
+    
+    public mutating func retain() {
+        
+        ReferenceCounter.queue.sync {
+            
+            _count += 1
+        }
+    }
+    
+    public mutating func release() {
+        
+        ReferenceCounter.queue.sync {
+
+            if _count > 0 {
+                
+                _count -= 1
+            }
+        }
+    }
+}
+
+extension ReferenceCounter {
+    
+    static let queue = DispatchQueue(label: "jp.ez-net.Ocean.ReferenceCounter")
+}
