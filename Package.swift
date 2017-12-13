85777c4c727317696814a6e33d38ae412b682e53
Tomohiro Kumagai <tomohiro@ez-style.jp>
2017-12-14 07:30:47 +0900

    Stop using swift package manager.

--- Package.swift
+++
@@ -1,8 +0,0 @@
-import PackageDescription
-
-let package = Package(
-    name: "Ocean",
-    dependencies: [
-        .Package(url: "https://github.com/es-kumagai/Swim", majorVersion: 0, minor: 1)
-    ]
-)
