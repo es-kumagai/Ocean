91c9cecbe577a3cdfa292368adf3c7148463f3cd
Tomohiro Kumagai <tomohiro@ez-style.jp>
2017-12-18 03:44:29 +0900

    Separated targets into iOS and macOS.

---
+++ Ocean_iOS/Ocean_iOS.h
@@ -0,0 +1,18 @@
+//
+//  Ocean_iOS.h
+//  Ocean_iOS
+//
+//  Created by Tomohiro Kumagai on 12/18/17.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Ocean_iOS.
+FOUNDATION_EXPORT double Ocean_iOSVersionNumber;
+
+//! Project version string for Ocean_iOS.
+FOUNDATION_EXPORT const unsigned char Ocean_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ocean_iOS/PublicHeader.h>
+
+
---
+++ Ocean_macOS/Ocean_macOS.h
@@ -0,0 +1,18 @@
+//
+//  Ocean_macOS.h
+//  Ocean_macOS
+//
+//  Created by Tomohiro Kumagai on 12/18/17.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Ocean_macOS.
+FOUNDATION_EXPORT double Ocean_macOSVersionNumber;
+
+//! Project version string for Ocean_macOS.
+FOUNDATION_EXPORT const unsigned char Ocean_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ocean_macOS/PublicHeader.h>
+
+
