e739b74b99916456e227a936daf9e3c6e76f614d
Tomohiro Kumagai <tomohiro@ez-style.jp>
2019-09-27 23:31:33 +0900

    Update for SwiftPM.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -24,5 +24,6 @@ let package = Package(
         .testTarget(
             name: "OceanTests",
             dependencies: ["Ocean"]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
