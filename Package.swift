1e1514e530bc8926736437bad2b48b5edc9c8470
Tomohiro Kumagai <tomohiro@ez-style.jp>
2021-04-30 18:08:33 +0900

    A unsafe flags is removed.

--- Package.swift
+++ Package.swift
@@ -25,10 +25,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Ocean",
-            dependencies: ["Swim"],
-            swiftSettings: [
-                .unsafeFlags(["-enable-library-evolution"]),
-            ]
+            dependencies: ["Swim"]
         ),
         .testTarget(
             name: "OceanTests",
