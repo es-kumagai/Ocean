4dbccdab277fff5fdedabee6db3974a1bb6baec8
Tomohiro Kumagai <tomo@ez-net.jp>
2022-09-21 19:43:53 +0900

    Support a feature of swift library evolution.

--- Package.swift
+++ Package.swift
@@ -24,7 +24,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Ocean",
-            dependencies: ["Swim"]
+            dependencies: ["Swim"],
+            swiftSettings: [
+                .define("-enable-library-evolution")
+            ]
         ),
         .testTarget(
             name: "OceanTests",
