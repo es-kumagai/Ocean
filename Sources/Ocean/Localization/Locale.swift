769671e1ba523ce6e937351091c659bb6c84b65a
Tomohiro Kumagai <tomo@ez-net.jp>
2024-08-23 00:19:53 +0900

    Add some locale constants.

---
+++ Sources/Ocean/Localization/Locale.swift
@@ -0,0 +1,24 @@
+//
+//  Locale.swift
+//
+//  
+//  Created by Tomohiro Kumagai on 2024/08/23
+//  
+//
+
+import Foundation
+
+public extension Locale {
+    
+    /// [Ocean] Represents the `en_US_POSIX` locale.
+    static let posix = Locale(identifier: "en_US_POSIX")
+    
+    /// [Ocean] Represents the `fr_FR` locale.
+    static let french = Locale(identifier: "fr_FR")
+    
+    /// [Ocean] Represents the `ja_JP` locale.
+    static let japanese = Locale(identifier: "ja_JP")
+    
+    /// [Ocean] Represents the `en_US` locale.
+    static let us = Locale(identifier: "en_US")
+}
